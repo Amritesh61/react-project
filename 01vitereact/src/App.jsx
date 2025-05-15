1b4a1e58f1e5fc96452d22a582cab88410750a38
Amritesh 61 <amritesh.raj61@gmail.com>
2025-05-15 17:03:41 +0530

    15/05/2025

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,11 +2,12 @@
 
 import React01 from './react01.jsx'
 function App() {
+  const username = "testing variable"
 
   return (
     <>
    <React01 />
-   <h1>REACT TESTING</h1>
+   <h1>REACT TESTING {username}</h1>
    <p> This is a test</p>
    </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,6 +1,20 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+
+function MyApp() {
+    return(
+        <div>
+            <h1>Custom App !!!</h1>
+        </div>
+    )
+}
+
+const reactElement = React.createElement(
+    'a',
+    {href : 'https://google.com' , target : '_blank'},
+    'Click Me to go to Google'
+)
 
 createRoot(document.getElementById('root')).render(
   
