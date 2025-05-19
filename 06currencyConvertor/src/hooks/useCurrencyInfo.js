6a1704febfb77b8a6fe2f900d06dad9daa3fc30c
Amritesh 61 <amritesh.raj61@gmail.com>
2025-05-19 17:51:53 +0530

    19/05/25

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,17 +1,45 @@
-import { useEffect,useState } from "react";
+// import { useState ,useEffect } from "react";
 
 
-function useCurrencyInfo(currency) {
-    useEffect (() => {
-        const[data, setData] = useState({})
-        fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
-        .then((response) => response.json())
-        .then((response) => 
-            setData(response[currency]))
-        console.log(data);
+// function useCurrencyInfo(base) {
+//     useEffect (() => {
+//         const[data, setData] = useState({})
+//         fetch(`https://api.exchangerate-api.com/v4/latest/${base}`)
+//         .then((response) => response.json())
+//         // .then((response) => 
+//         //     setData(response[currency]))
+//         // console.log(data);
+//         .then(json => {
+//         // json has a "rates" object containing all currency pairs
+//         setData(json.rates);
         
-    }, [currency])
-    console.log(data);
-    return data
+//     })
+//     .catch(console.error);
+// }, [base]);
+    
+//     return data
+// }
+// export default useCurrencyInfo;
+
+
+// src/hooks/useCurrencyInfo.js
+import { useState, useEffect } from 'react';
+
+export default function useCurrencyInfo(currency) {
+  // 1) Declare your state at the top level
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    // 2) Fetch & set inside useEffect
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+      .then(res => res.json())
+      .then(json => {
+        // pull out the rates object
+        setData(json.rates);
+      })
+      .catch(err => console.error('Currency fetch failed:', err));
+  }, [currency]);
+
+  // 3) Now data is defined here
+  return data;
 }
-export default useCurrencyInfo;
