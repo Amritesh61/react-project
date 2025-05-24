33b79b9b0d1f6ed331b7392ffdd84fbedfc008b1
Amritesh61 <amritesh.raj61@gmail.com>
2025-05-24 22:52:17 +0530

    24-05-25/3rd

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,4 +1,4 @@
-import {  useState } from 'react'
+import {  useState ,useEffect, use } from 'react'
 import {TodoProvider} from './contexts'
 import './App.css'
 
@@ -22,10 +22,20 @@ function App() {
     setTodos((prev) => prev.map((prevTodo) => 
       prevTodo === id ? 
          { ...prevTodo, completed: !prevTodo.completed }
-          : prevTodo ))
-      
-    
+          : prevTodo )) 
   }
+
+  useEffect(() => { 
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [] )
+
+  useEffect(() => {
+
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
 
 
   return (
