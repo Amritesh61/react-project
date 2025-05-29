38b8ff6a482d0823d9e28aae20963ee0cab19012
Amritesh 61 <amritesh.raj61@gmail.com>
2025-05-29 16:32:43 +0530

    29-05-25/4th

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -2,7 +2,32 @@ import conf from '../conf/conf.js'
 
 import { Client, Account , ID} from "appwrite";
 
-export class AuthService {}
+export class AuthService {
+    client = new Client();
+    account ;
+
+constructor() {
+    this.client
+        .setEndpoint(conf.appwriteUrl) // Your Appwrite Endpoint
+        .setProject(conf.appwriteProjectId); // Your Appwrite Project ID
+    this.account = new Account(this.client);    
+    }
+
+    async createAccount({ email , password , name}){
+        try{
+            await this.account
+                .create(ID.unique() , email, password, name);
+                if(userAccount){
+                    return userAccount;
+                }else {
+                    return
+                }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+}
 
 const authService = new AuthService();
 
