5d6c003973795d7749c892f068362b5e040121ff
Amritesh 61 <amritesh.raj61@gmail.com>
2025-05-29 16:53:31 +0530

    29-05-25/5th

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -18,16 +18,44 @@ constructor() {
             await this.account
                 .create(ID.unique() , email, password, name);
                 if(userAccount){
-                    return userAccount;
+                    return this.login({ email, password });
                 }else {
-                    return
+                    return userAccount;
                 }
 
         } catch (error) {
             throw error;
         }
     }
-}
+    async login ({ email, password }) {
+        try {
+          return await this.account.createEmailPasswordSession(email, password);
+
+        }catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.error("Appwrite service :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try{
+            await this.account.deleteSessions();
+
+        }catch(error){
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+
+}   
 
 const authService = new AuthService();
 
