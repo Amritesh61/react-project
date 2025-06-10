c136d65a3720f126b630666e33656c4063d0b24e
Amritesh 61 <amritesh.raj61@gmail.com>
2025-06-10 18:06:28 +0530

    10/06/25-7

--- 12MegaBlog/src/main.jsx
+++ 12MegaBlog/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Route } from 'react-router-dom'
 import { AuthLayout, Login } from './components/index.js'
 
 const router = createBrowserRouter([
@@ -70,7 +70,7 @@ const router = createBrowserRouter([
  ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App /> 
+    <RouterProvider router={router}/>
     </ Provider>
   </React.StrictMode>,
  )
