c63d3a58c0a92cfa24d2fb23037a63b0e3dfb45a
Amritesh 61 <amritesh.raj61@gmail.com>
2025-06-16 18:05:16 +0530

    4th

---
+++ PersonalPortfolio/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import { cn } from "@/lib/utils";
+import { use, useEffect } from "react";
+
+const navItems = [ 
+    { name: "Home", href: "#hero" },
+    { name: "About", href: "#about" },
+    { name: "Skills", href: "#skills" },
+    { name: "Projects", href: "#projects" },
+    { name: "Contact", href: "#contact" },
+];
+
+export const Navbar = () => { 
+    const[ isScrolled, setIsScrolled ] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 10);
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+    return <nav className = {cn("fixed w-full z-40 transition-all duration-300",
+        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-x5" : "py-5"
+    )} >
+
+        <div className="container flex items-center justify-between"> 
+            <a>
+                <span>
+                    <span> Amritesh </span> Portfolio
+                </span>
+            </a>
+
+        </div>
+
+    </nav>
+};
